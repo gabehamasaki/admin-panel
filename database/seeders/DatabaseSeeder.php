55033fcc1f44a73a44ebe1ed87139904c42dc958
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2023-10-10 05:42:37 -0300

    feat: add super admin seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $password = Str::random(8);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Administrador',
+            'email' => 'admin@exata.it',
+            'password' => bcrypt($password),
+            'role' => 'superadmin',
+        ]);
+
+        print('----------------------------------------------------------------');
+        print('Admin password: ' . $password);
+        print('');
     }
 }
